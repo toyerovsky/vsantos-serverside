e736d7b298d50b2c0e77328282174fbd3dd7c8b7
Przemysław Postrach <przemyslaw.postrach@hotmail.com>
2018-03-07 19:09:33 +0100

    Delete bin and obj folders

---
+++ src/serverside/Core/ValidationHelper.cs
@@ -0,0 +1,23 @@
+﻿/* Copyright (C) Przemysław Postrach - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Przemysław Postrach <przemyslaw.postrach@hotmail.com> December 2017
+ */
+
+namespace Serverside.Core
+{
+    public static class ValidationHelper
+    {
+        public static bool IsMoneyValid(decimal moneyToCheck) => moneyToCheck >= 0m;
+
+        public static bool IsGroupSlotValid(string groupSlot) =>
+            byte.TryParse(groupSlot, out byte slot) && IsGroupSlotValid(slot);
+       
+        public static bool IsCellphoneNumberValid(string number)
+        {
+            return int.TryParse(number, out int converted) && converted.ToString().Length > 0 && converted.ToString().Length <= 6;
+        }
+
+        public static bool IsGroupSlotValid(byte slot) => slot <= 3 && slot >= 1;
+    }
+}
