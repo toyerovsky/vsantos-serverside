63c030d98aca1336475c0fe5f417c950469b8851
Przemysław Postrach <przemyslaw.postrach@hotmail.com>
2018-05-14 23:05:51 +0200

    Added ZoneType

--- src/core/Database/Models/ZoneModel.cs
+++ src/core/Database/Models/ZoneModel.cs
@@ -1,10 +1,15 @@
-﻿namespace VRP.Core.Database.Models
+﻿using System.ComponentModel.DataAnnotations;
+using VRP.Core.Enums;
+
+namespace VRP.Core.Database.Models
 {
     public class ZoneModel
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public int? CreatorId { get; set; }
+        [EnumDataType(typeof(ZoneType))]
+        public virtual ZoneType ZoneType { get; set; }
         public string ZonePropertiesJson { get; set; }
     }
 }
