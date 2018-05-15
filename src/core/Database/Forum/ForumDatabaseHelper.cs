1e4f03cd405af70d0eeda1d837e6514dd5915d52
toyerovsky <przemyslaw.postrach@hotmail.com>
2018-05-15 20:48:54 +0200

    Added configuration to prevent errors in ForumDatabaseHelper

--- src/core/Database/Forum/ForumDatabaseHelper.cs
+++ src/core/Database/Forum/ForumDatabaseHelper.cs
@@ -4,53 +4,52 @@
  * Written by V Role Play team <contact@v-rp.pl> December 2017
  */
 
-using System;
 using System.Data;
-using System.Threading.Tasks;
 using Dapper;
 using Microsoft.Extensions.Configuration;
 using MySql.Data.MySqlClient;
-using VRP.Core.Tools;
 
 namespace VRP.Core.Database.Forum
 {
-    public static class ForumDatabaseHelper
+    public class ForumDatabaseHelper
     {
-        public static bool CheckPasswordMatch(string email, string password, out ForumUser forumUser)
+        private readonly IConfiguration _configuration;
+
+        public ForumDatabaseHelper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public bool CheckPasswordMatch(string email, string password, out ForumUser forumUser)
         {
             forumUser = null;
 
             if (!email.Contains("@"))
                 return false;
 
-
             using (IDbConnection connection =
-                new MySqlConnection(Singletons.Configuration.GetConnectionString("forumConnectionString")))
+                new MySqlConnection(_configuration.GetConnectionString("forumConnectionString")))
             {
-                var doesUserExistQuery = "SELECT COUNT(member_id) FROM core_members WHERE email = @email";
-                int count = connection.QueryFirst<int>(doesUserExistQuery, new { email });
+                var query = "SELECT member_id as Id," +
+                            " Name as UserName," +
+                            " members_pass_hash as PasswordHash," +
+                            " members_pass_salt as PasswordSalt," +
+                            " member_group_id as GroupId," +
+                            " mgroup_others as OtherGroups FROM core_members WHERE email = @email";
 
-                if (count > 0)
+                forumUser = connection.QueryFirstOrDefault<ForumUser>(query, new { email });
+
+                if (forumUser != null)
                 {
-                    var query = "SELECT member_id as Id," +
-                                " Name as UserName," +
-                                " members_pass_hash as PasswordHash," +
-                                " members_pass_salt as PasswordSalt," +
-                                " member_group_id as GroupId," +
-                                " mgroup_others as OtherGroups FROM core_members WHERE email = @email";
-
-                    forumUser = connection.QueryFirst<ForumUser>(query, new { email });
-
                     string hash = GenerateIpbHash(password, forumUser.PasswordSalt);
-
                     return hash == forumUser.PasswordHash;
                 }
-            }
 
-            return false;
+                return false;
+            }
         }
 
-        private static string GenerateIpbHash(string plainText, string salt)
+        private string GenerateIpbHash(string plainText, string salt)
         {
             return BCrypt.Net.BCrypt.HashPassword(plainText, "$2a$13$" + salt);
         }
