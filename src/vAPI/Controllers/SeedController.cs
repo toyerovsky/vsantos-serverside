8c9aa19a86a5f7bbe0659d6a5d0f8ec3cccf3278
toyerovsky <przemyslaw.postrach@hotmail.com>
2018-06-14 18:37:55 +0200

    Add account seed

---
+++ src/vAPI/Controllers/SeedController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using VRP.Core.Database.Models.Account;
+using VRP.Core.Enums;
+using VRP.Core.Repositories;
+
+namespace VRP.vAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class SeedController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public SeedController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost("seedaccounts")]
+        public IActionResult Post()
+        {
+            var query = "SELECT member_id as ForumUserId," +
+                        " name as Name," +
+                        " member_group_id as PrimaryForumGroup,`" +
+                        " email as Email FROM core_members";
+
+            using (IDbConnection connection = new MySqlConnection(
+                _configuration.GetConnectionString("forumConnectionString")))
+            {
+                using (AccountsRepository accountsRepository = new AccountsRepository())
+                using (var multiple = connection.QueryMultiple(query))
+                {
+                    foreach (var account in multiple.Read<AccountModel>())
+                    {
+                        account.ServerRank = (ServerRank)account.PrimaryForumGroup;
+                        accountsRepository.Insert(account);
+                    }
+                    accountsRepository.Save();
+                }
+            }
+            return Ok();
+        }
+    }
+}
