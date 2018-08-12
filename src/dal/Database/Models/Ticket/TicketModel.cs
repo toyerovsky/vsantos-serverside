888bb2d48cd70031d52d428ed4023f0a214a6510
bezel <dawids14@onet.pl>
2018-08-12 18:43:49 +0200

    Add TicketModel,TicketMessageModel and TicketRepository

---
+++ src/dal/Database/Models/Ticket/TicketModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+
+namespace VRP.Database.Models.Ticket
+{
+    public class TicketModel
+    {
+        public InformationContainer()
+        {
+            MessageContent = new HashSet<TicketMessageModel>();
+            InvolvedAccounts = new HashSet<AccountModel>();
+            InvolvvedAdmins = new HashSet<AccountModel>();
+        }
+        
+        
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Type { get; set; }
+        public bool Closed { get; set; }
+
+        public virtual ICollection<TicketMessageModel> MessageContent { get; set; }
+        public virtual ICollection<AccountModel> InvolvedAccounts { get; set; }
+        public virtual ICollection<AccountModel> InvolvedAdmins { get; set; }
+
+
+    }
+}
+
---
+++ src/dal/Enums/TicketType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace VRP.DAL.Enums
+{
+    public enum TicketType
+    {
+        [Description("Bug na forum")]
+        BugForum,
+        [Description("Bug w grze")]
+        BugGame,
+        [Description("Wniosek FCK")]
+        Fck,
+        [Description("Wniosek FCJ")]
+        Fcj,
+        [Description("Inne")]
+        Other
+    }
+}
