8235987028845125a1f39279a7c0e7aea4f73274
Przemysław P <toyerek@gmail.com>
2018-08-20 22:56:33 +0200

    Add props to BuildingDto

--- src/vAPI/Dto/BuildingDto.cs
+++ src/vAPI/Dto/BuildingDto.cs
@@ -1,10 +1,19 @@
-﻿namespace VRP.vAPI.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VRP.vAPI.Dto
 {
     public class BuildingDto
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public bool SpawnPossible { get; set; }
+        public float ExternalPickupPositionX { get; set; }
+        public float ExternalPickupPositionY { get; set; }
+        public float ExternalPickupPositionZ { get; set; }
+        public float InternalPickupPositionX { get; set; }
+        public float InternalPickupPositionY { get; set; }
+        public float InternalPickupPositionZ { get; set; }
         public CharacterDto Character { get; set; }
         public int CharacterId { get; set; }
         public GroupDto Group { get; set; }
