6889ce1d938c54e49eacd7bd2f75624d82466b97
Przemysław P <toyerek@gmail.com>
2018-08-30 15:10:45 +0200

    Add roles to appsettings, role helper

---
+++ src/vAPI/Helpers/RoleHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using Microsoft.Extensions.Configuration;
+
+namespace VRP.vAPI.Helpers
+{
+    public static class RoleHelper
+    {
+        public static string[] SupportRanks = Startup.Configuration.GetValue<string[]>("SupportRanks");
+        public static string[] AdminRoles = Startup.Configuration.GetValue<string[]>("AdminRoles");
+        public static string[] DevRoles = Startup.Configuration.GetValue<string[]>("DevRoles");
+        public static string[] ManagementRoles = Startup.Configuration.GetValue<string[]>("ManagementRoles");
+
+        public static string[] GetFromSupportRoles()
+        {
+            return SupportRanks.Concat(AdminRoles).Concat(DevRoles).Concat(ManagementRoles).ToArray();
+        }
+
+        public static string[] GetFromAdminRoles()
+        {
+            return AdminRoles.Concat(DevRoles).Concat(ManagementRoles).ToArray();
+        }
+
+        public static string[] GetFromDevRoles()
+        {
+            return DevRoles.Concat(ManagementRoles).ToArray();
+        }
+    }
+}
