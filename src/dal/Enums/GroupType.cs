6fa7217419388db301419fb90240188d9fafb484
Przemysław P <toyerek@gmail.com>
2018-09-09 21:41:45 +0200

    Add descriptions to GroupType enum

--- src/dal/Enums/GroupType.cs
+++ src/dal/Enums/GroupType.cs
@@ -4,19 +4,27 @@
  * Written by V Role Play team <contact@v-rp.pl> December 2017
  */
 
+using System.ComponentModel;
+
 namespace VRP.DAL.Enums
 {
     public enum GroupType
     {
+        [Description("Taxi")]
         Taxi,
+        [Description("Bar")]
         Bar,
+        [Description("Klub")]
         Club,
-        Store,
+        [Description("Organizacja przestępcza")]
         Crime,
+        [Description("Warsztat")]
         Workshop,
+        [Description("Policja")]
         Police,
+        [Description("Szpital")]
         Hospital,
-        News,
-        CityHall
+        [Description("Wiadomości")]
+        News
     }
 }
