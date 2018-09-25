1a73f6c8263639f5b48a106695ba46720cd3a92d
bezel <dawids14@onet.pl>
2018-09-25 22:52:59 +0200

    Add various improvements

---
+++ src/VRP.BLL/Dto/CrimeBotDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using VRP.DAL.Database.Models.Account;
+using VRP.DAL.Database.Models.Bot;
+using VRP.DAL.Database.Models.Group;
+
+namespace VRP.BLL.Dto
+{
+    public class CrimeBotDto
+    {
+        public int Id { get; set; }
+        public string VehicleModel { get; set; }
+        public int CreatorId { get; set; }
+        public int GroupModelId { get; set; }
+        public int BotModelId { get; set; }
+
+    }
+}
---
+++ src/VRP.BLL/Dto/CrimeBotItemDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace VRP.BLL.Dto
+{
+    public class CrimeBotItemDto
+    {
+        public int Id { get; set; }
+        public decimal Cost { get; set; }
+        public int Count { get; set; }
+        public int ResetCount { get; set; }
+        public int ItemTemplateModelId { get; set; }
+        public ItemTemplateDto ItemTemplate { get; set; }
+    }
+}
